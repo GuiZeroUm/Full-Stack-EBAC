8451f2d7042679d45e73f50aeee7f92d66e98084
GuiZeroUm <guilherme2004official@hotmail.com>
2023-12-17 16:14:38 -0500

    feat: add jQuery functions and animations

--- aulas/jQuey/script.js
+++ aulas/jQuey/script.js
@@ -9,5 +9,16 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none;"></li>');
+        $(`<img src='${enderecoDaNovaImagem}'/>`).appendTo(novoItem);
+        $(`<div class='overlay-img'/div>
+        <a href=${enderecoDaNovaImagem}' target='_blank' title='Ver imagem em tamanho real'>
+        Ver imagem em tamanho real
+        </a>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
     })
 })
