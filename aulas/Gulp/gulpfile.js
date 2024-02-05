538c4bfad3b9c926344acb37a9349939a381aa53
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2024-02-05 12:01:39 -0500

    gulp series

--- aulas/Gulp/gulpfile.js
+++ aulas/Gulp/gulpfile.js
@@ -1,17 +1,22 @@
+//importando o gulp
+const gulp = require('gulp');
+
 function funcaoPadrao(callback) {
     console.log ('executando via gulp');
     callback();
 }
 
+//função publica (tem callback)
 function dizOi(callback) {
     console.log('olá gulp');
     dizTchau();
     callback();
 }
 
+//função privada (não tem callback)
 function dizTchau() {
     console.log('tchau gulp');
 }
-
-exports.default = funcaoPadrao;
+//fazendo exportação em série com o gulp.series
+exports.default = gulp.series(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
