1e43c702e305296f63298a5653cb583cd31ae504
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2024-02-13 16:28:27 -0500

    tarefas com Grunt

--- aulas/Grunt/GruntFile.js
+++ aulas/Grunt/GruntFile.js
@@ -1,5 +1,18 @@
+const { set } = require("grunt");
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
     })
+
+    grunt.registerTask('helloWorld', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log('Hello World');
+            done();
+        }, 3000);
+    })
+
+    grunt.registerTask('default', ['helloWorld'])
+        
 }
