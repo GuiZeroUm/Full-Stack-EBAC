bba93d18feb045d01d3c437964e4b3427a505ba7
GuiZeroUm <guilherme2004official@hotmail.com>
2024-03-12 22:25:09 -0500

    atividade ajax finalizada

---
+++ Projetos/exercicio_ajax/ajax.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name')
+    const usernameElement = document.querySelector('#username')
+    const avatarElement = document.querySelector('#avatar')
+    const reposElement = document.querySelector('#repos')
+    const followersElement = document.querySelector('#followers')
+    const followingElement = document.querySelector('#following')
+    const linkElement = document.querySelector('#link')
+
+    const link = 'https://api.github.com/users/GuiZeroUm'
+
+    fetch(link)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        nameElement.innerHTML = json.name
+        usernameElement.innerHTML = json.login
+        avatarElement.src = json.avatar_url
+        reposElement.innerHTML = json.public_repos
+        followersElement.innerHTML = json.followers
+        followingElement.innerHTML = json.following
+        linkElement.href = json.html_url
+    })
+})
