e88abf9af959dc32cb82fa401802d86ef94b41de
GuiZeroUm <guilherme2004official@hotmail.com>
2024-03-16 15:45:45 -0500

    encapsulamento

--- aulas/POO/funcionario.js
+++ aulas/POO/funcionario.js
@@ -8,10 +8,23 @@ function Pessoa(nome) {
 
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo
-    this.salario = salario
+    // this.salario = salario
+    let _salario = salario
+    
+    // pega salario (getters)
+    this.getSalario = function() {
+        return `O salário de ${this.nome} é ${_salario}`
+    }
 
     this.dizCargo = function() {
         console.log(this.cargo)
+    }
+
+    //atribui salario (setters)
+    this.setSalario = function(valor) {
+        if (typeof valor === Number) {
+            _salario = valor
+        }
     }
 
     Pessoa.call(this, nome)
@@ -20,3 +33,7 @@ function Funcionario(nome, cargo, salario) {
 const Funcionario1 = new Funcionario('Maria', 'Dev', 5000)
 Funcionario1.dizOi()
 Funcionario1.dizCargo()
+
+Funcionario1.setSalario('mil') 
+
+console.log (Funcionario1.getSalario())
