ece122f2a79a9efe49d49b0953091e9c2efb60af
GuiZeroUm <guilherme2004official@hotmail.com>
2024-03-28 18:47:23 -0500

    condicionais em vue

--- aulas/VueJS/vue-project/src/App.vue
+++ aulas/VueJS/vue-project/src/App.vue
@@ -11,8 +11,14 @@
   }
 
   const enderecoDaImagem = 'https://sm.ign.com/ign_br/screenshot/default/produtor-de-castlevania-quer-animar-berserk_tk9a.jpg'
+  const imagemFrieren = 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/01/03/frieren-e-a-jornada-para-o-alem-qhvlvd2l8mr5.jpg'
 
   const botaoEstaDesabilitado = true
+
+  const gostaDeBerser = false
+  const gostaDaFrieren = true
+
+  const estaAutorizado = true
 </script>
 
 <template>
@@ -21,9 +27,16 @@
   <button :disabled="botaoEstaDesabilitado">Teste</button>
 
   <h1>Eu gosto de {{ meuObj.animeFavorito }}</h1>
-  <img :src="enderecoDaImagem" alt="">
+  <img v-if="gostaDeBerser" :src="enderecoDaImagem" alt="">
+  <img :src="imagemFrieren" v-else-if="gostaDaFrieren" alt="">
+  <h2 v-else>Não curte os foda</h2>
+
+  <h1 v-if="estaAutorizado">Bem-Vindo</h1>
+  <h1 v-else>Não possui acesso</h1>
 </template>
 
 <style scoped>
-
+  img {
+    max-width: 800px;
+  }
 </style>
