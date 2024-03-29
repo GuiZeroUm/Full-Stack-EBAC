c61f26697c2671919674fbfa1e6cf17dbc1cf23a
GuiZeroUm <guilherme2004official@hotmail.com>
2024-03-29 17:50:56 -0500

    manipulando dados

--- aulas/VueJS/vue-project/src/App.vue
+++ aulas/VueJS/vue-project/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { reactive } from 'vue';
+
   const nome = 'Guilherme'
 
   const meuObj = {
@@ -19,20 +21,50 @@
   const gostaDaFrieren = true
 
   const estaAutorizado = true
+
+  // let contador = 0
+  const estado = reactive({
+    contador: 0
+  })
+
+  const incrementar = () => {
+    estado.contador++
+  }
+
+  const decrementar = () => {
+    estado.contador--
+  }
+
+  const alteraEmail = evento => estado.email = evento.target.value
 </script>
 
 <template>
   <h1>Olá {{ dizOi() }}</h1>
 
-  <button :disabled="botaoEstaDesabilitado">Teste</button>
-
+  
   <h1>Eu gosto de {{ meuObj.animeFavorito }}</h1>
   <img v-if="gostaDeBerser" :src="enderecoDaImagem" alt="">
   <img :src="imagemFrieren" v-else-if="gostaDaFrieren" alt="">
   <h2 v-else>Não curte os foda</h2>
-
+  
   <h1 v-if="estaAutorizado">Bem-Vindo</h1>
   <h1 v-else>Não possui acesso</h1>
+
+  <button :disabled="botaoEstaDesabilitado">Teste</button>
+
+  <br />
+  <hr />
+
+  {{ estado.contador }}
+
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br />
+  <hr />
+
+  {{ estado.email }}
+  <input type="email" @keyup="alteraEmail">
 </template>
 
 <style scoped>
